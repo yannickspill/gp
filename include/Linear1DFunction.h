b733736e3b6d00db5e834983b2da5789a0bc1115
Yannick <yannick.spill@pasteur.fr>
2013-11-14 22:59:44 -0800

    cast Vector1D to double

--- include/Linear1DFunction.h
+++ include/Linear1DFunction.h
@@ -1,6 +1,9 @@
 #ifndef LINEAR1D_FUNCTION_H
 #define LINEAR1D_FUNCTION_H
 
+#include "Scalar.h"
+#include <Eigen/Dense>
+
 //! Linear one-dimensional function
 /* f(x) = a*x + b
  * a,b : Scalars
@@ -8,16 +11,22 @@
  * x : double (or Vector1D)
  */
 class Linear1DFunction {
-    Scalar* a_, *b_;
+    Scalar *a_, *b_;
 
    public:
     Linear1DFunction(Scalar* a, Scalar* b) : a_(a), b_(b) {}
 
-    double operator()(double x) { return get_a_value() * x + get_b_value(); }
+    double operator()(double x) const {
+        return get_a_value() * x + get_b_value();
+    }
+
+    double operator()(Eigen::Matrix<double, 1, 1> x) const {
+        return (*this)(x(0));
+    }
 
    private:
-    double get_a_value() { return a_->get(); }
-    double get_b_value() { return b_->get(); }
+    double get_a_value() const { return a_->get(); }
+    double get_b_value() const { return b_->get(); }
 };
 
 #endif /* LINEAR1D_FUNCTION_H */
