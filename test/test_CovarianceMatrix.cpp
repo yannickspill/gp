ea4898a4fb1003a18cd7c8c1541753291bfc5736
Yannick <yannick.spill@pasteur.fr>
2013-11-15 18:33:11 -0800

    add CovarianceMatrix

---
+++ test/test_CovarianceMatrix.cpp
@@ -0,0 +1,21 @@
+#include "CovarianceMatrix.h"
+#include <Eigen/Dense>
+
+class ConstantCov {
+   public:
+    double operator()(Eigen::Matrix<double, 1, 1> x,
+                      Eigen::Matrix<double, 1, 1> y) const {
+        return 1.;
+    }
+};
+
+int main(int, char * []) {
+    Eigen::MatrixXd X(Eigen::MatrixXd::Random(10, 3));
+    ConstantCov cc;
+    CovarianceMatrix<ConstantCov> cov(X, cc);
+    Eigen::MatrixXd observed(cov());
+    Eigen::MatrixXd expected(Eigen::MatrixXd::Ones(10,10));
+    if (observed != expected) return 1;
+    return 0;
+}
+
