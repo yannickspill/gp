2bde939baa3ebe6995f43686e4411d6783d33ad9
Yannick <yannick.spill@pasteur.fr>
2013-11-15 16:28:14 -0800

    Scalar is now a Handle
    it just stores a shared_ptr to data on the heap

--- include/Scalar.h
+++ include/Scalar.h
@@ -1,47 +1,56 @@
 #ifndef SCALAR_H
 #define SCALAR_H
 
+#include <limits>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
 // Scalar with optional upper/lower bounds
 class Scalar {
    public:
-    Scalar(double value)
-        : value_(value), has_upper_(false), has_lower_(false) {};
+    explicit Scalar(double value,
+                    double lower = -std::numeric_limits<double>::infinity(),
+                    double upper = std::numeric_limits<double>::infinity())
+        : data_(boost::make_shared<Data>(value, lower, upper)) {}
 
-    bool has_lower() const { return has_lower_; }
-    bool has_upper() const { return has_upper_; }
+    Scalar(const Scalar& other) : data_(other.data_) {}
 
-    void clear_lower() { has_lower_ = false; }
-    void clear_upper() { has_upper_ = false; }
+    void clear_lower() {
+        data_->lower_ = -std::numeric_limits<double>::infinity();
+    }
+    void clear_upper() {
+        data_->upper_ = std::numeric_limits<double>::infinity();
+    }
 
     void set_lower(double lower) {
-        lower_ = lower;
-        if (has_upper() and(get_upper() < lower_)) set_upper(lower);
-        has_lower_ = true;
-        if (get() < lower_) set(lower_);
+        data_->lower_ = lower;
+        if (get_upper() < lower) set_upper(lower);
+        if (get() < lower) set(lower);
     }
     void set_upper(double upper) {
-        upper_ = upper;
-        if (has_lower() and(get_lower() > upper_)) set_lower(upper);
-        has_upper_ = true;
-        if (get() > upper_) set(upper_);
+        data_->upper_ = upper;
+        if (get_lower() > upper) set_lower(upper);
+        if (get() > upper) set(upper);
     }
 
-    double get_lower() { return lower_; }
-    double get_upper() { return upper_; }
+    double get_lower() const { return data_->lower_; }
+    double get_upper() const { return data_->upper_; }
 
-    double get() { return value_; }
+    double get() const { return data_->value_; }
 
     void set(double value) {
         double target = value;
-        if (has_lower() and(get_lower() > value)) target = get_lower();
-        if (has_upper() and(get_upper() < value)) target = get_upper();
-        value_ = target;
+        if (get_lower() > value) target = get_lower();
+        if (get_upper() < value) target = get_upper();
+        data_->value_ = target;
     }
 
    private:
-    double value_;
-    bool has_upper_, has_lower_;
-    double upper_, lower_;
+    struct Data {
+        double value_, lower_, upper_;
+        Data(double v, double l, double u) : value_(v), lower_(l), upper_(u) {}
+    };
+    boost::shared_ptr<Data> data_;
 };
 
 #endif /* SCALAR_H */
--- test/test_Scalar.cpp
+++ test/test_Scalar.cpp
@@ -1,34 +1,45 @@
 #include "Scalar.h"
 
+#include <limits>
+
 int main(int, char * []) {
     Scalar s(1.0);
+    double inf = std::numeric_limits<double>::infinity();
     if (s.get() != 1.0) return 1;
-    if (s.has_lower() == true) return 2;
-    if (s.has_upper() == true) return 3;
+    if (s.get_lower() != -inf) return 2;
+    if (s.get_upper() != inf) return 3;
 
     s.set_lower(2.0);
     if (s.get() != 2.0) return 4;
-    if (s.has_lower() == false) return 5;
-    if (s.has_upper() == true) return 6;
+    if (s.get_lower() != 2.0) return 5;
+    if (s.get_upper() != inf) return 6;
 
     s.clear_lower();
     if (s.get() != 2.0) return 7;
-    if (s.has_lower() == true) return 8;
-    if (s.has_upper() == true) return 9;
+    if (s.get_lower() != -inf) return 8;
+    if (s.get_upper() != inf) return 9;
 
     s.set_upper(0.0);
     if (s.get() != 0.0) return 10;
-    if (s.has_lower() == true) return 11;
-    if (s.has_upper() == false) return 12;
+    if (s.get_lower() != -inf) return 11;
+    if (s.get_upper() != 0.0) return 12;
 
     s.clear_upper();
     if (s.get() != 0.0) return 13;
-    if (s.has_lower() == true) return 14;
-    if (s.has_upper() == true) return 15;
+    if (s.get_lower() != -inf) return 14;
+    if (s.get_upper() != inf) return 15;
 
+    //one bound moves the other one
     s.set_lower(3.0);
     s.set_upper(2.0);
     if (s.get_lower() != s.get_upper()) return 16;
+
+    //a scalar is just a handle on its data
+    Scalar t(s);
+    t.set(2.5);
+    if (s.get() != t.get()) return 17;
+    if (s.get_lower() != t.get_lower()) return 18;
+    if (s.get_upper() != t.get_upper()) return 19;
 
     return 0;
 }
