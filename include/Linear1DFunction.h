8b0d0abfac46e045ecda3ee3efbcbfb3527620b4
Yannick <yannick.spill@pasteur.fr>
2013-11-15 16:29:56 -0800

    Fix usage of Scalar

--- include/Linear1DFunction.h
+++ include/Linear1DFunction.h
@@ -11,22 +11,21 @@
  * x : double (or Vector1D)
  */
 class Linear1DFunction {
-    Scalar *a_, *b_;
+    Scalar a_, b_;
 
    public:
-    Linear1DFunction(Scalar* a, Scalar* b) : a_(a), b_(b) {}
+    Linear1DFunction(Scalar a, Scalar b) : a_(a), b_(b) {}
 
     double operator()(double x) const {
         return get_a_value() * x + get_b_value();
     }
-
     double operator()(Eigen::Matrix<double, 1, 1> x) const {
-        return (*this)(x(0));
+        return operator()(x(0));
     }
 
    private:
-    double get_a_value() const { return a_->get(); }
-    double get_b_value() const { return b_->get(); }
+    double get_a_value() const { return a_.get(); }
+    double get_b_value() const { return b_.get(); }
 };
 
 #endif /* LINEAR1D_FUNCTION_H */
--- test/test_Linear1DFunction.cpp
+++ test/test_Linear1DFunction.cpp
@@ -5,7 +5,7 @@
 int main(int, char * []) {
     Scalar a(1.0);
     Scalar b(3.0);
-    Linear1DFunction f(&a, &b);
+    Linear1DFunction f(a, b);
     if (f(1) != 4.) return 1;
     b.set(2.);
     if (f(3) != 5.) return 2;
