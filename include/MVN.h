faf20a53e0f946c1c30152a861804364c91c4cb5
Yannick <yannick.spill@pasteur.fr>
2013-11-20 12:57:36 -0800

    add derivatives wrt FM, FX and Sigma

--- include/MVN.h
+++ include/MVN.h
@@ -20,20 +20,56 @@ class MVN {
     double lJF_;
     MATRIX Sigma_;
 
+    typedef MatrixDifference<VECTORX, VECTORM> MD;
+
    public:
     MVN(VECTORX FX, VECTORM FM, double lJF, MATRIX Sigma)
         : FX_(FX), FM_(FM), lJF_(lJF), Sigma_(Sigma) {}
 
     double get() const { return evaluate(); }
 
+    Eigen::VectorXd get_derivative_FX() const { return -deriv_FM(); }
+    Eigen::VectorXd get_derivative_FM() const { return deriv_FM(); }
+
+    Eigen::MatrixXd get_derivative_Sigma() const { return deriv_Sigma(); }
+
+    void set_FX(VECTORX FX) { FX_(FX); }
+    void set_FM(VECTORM FM) { FM_(FM); }
+    void set_lJF(VECTORM lJF) { lJF_(lJF); }
+    void set_Sigma(MATRIX Sigma) { Sigma_(Sigma); }
+
    private:
     double evaluate() const {
         //first we transform the inputs to something MVNEvaluate can process
-        typedef MatrixDifference<VECTORX, VECTORM> MD;
         MD eps(FX_, FM_);
         LDLT<MATRIX> ldlt(Sigma_);
-        MVNEvaluate<MD, LDLT<MATRIX> > eval(eps, ldlt);
-        return eval.get() + lJF_;
+        Eigen::VectorXd epsilon(eps.get());
+        Eigen::VectorXd Peps(ldlt.solve(epsilon));
+        double exponent = epsilon.transpose()*Peps;
+        double lnorm = ldlt.get_log_determinant();
+        lnorm = epsilon.rows()*0.5*std::log(2*M_PI) + 0.5*lnorm;
+        return lnorm + 0.5 * exponent + lJF_;
+    }
+
+    Eigen::VectorXd deriv_FM() const {
+        // d(-log(p))/d(FM) = - Sigma^{-1} * epsilon
+        MD eps(FX_, FM_);
+        LDLT<MATRIX> ldlt(Sigma_);
+        Eigen::VectorXd epsilon(eps.get());
+        Eigen::VectorXd Peps(ldlt.solve(epsilon));
+        return -Peps;
+    }
+
+    Eigen::MatrixXd deriv_Sigma() const {
+        // d(-log(p))/dSigma = 1/2 (P -  P epsilon transpose(epsilon) P)
+        MD eps(FX_, FM_);
+        LDLT<MATRIX> ldlt(Sigma_);
+        Eigen::VectorXd epsilon(eps.get());
+        Eigen::VectorXd Peps(ldlt.solve(epsilon));
+        Eigen::MatrixXd Id(
+            Eigen::MatrixXd::Identity(epsilon.rows(), epsilon.rows()));
+        Eigen::MatrixXd P(ldlt.solve(Id));
+        return 0.5*( P - Peps*Peps.transpose() );
     }
 };
 
--- test/test_MVN.cpp
+++ test/test_MVN.cpp
@@ -97,6 +97,20 @@ bool test_degenerate() {
         if (naeq(observed, expected)) FAIL("evaluate");
     }
 
+    {
+        // evaluate_derivative_FM
+        double observed = mv.get_derivative_FM()(0);
+        double expected = fn.evaluate_derivative_FM();
+        if (naeq(observed, expected)) FAIL("evaluate_derivative_FM");
+    }
+
+    {
+        // evaluate_derivative_Sigma
+        double observed = mv.get_derivative_Sigma()(0, 0);
+        double expected = fn.evaluate_derivative_sigma();
+        expected = expected / (2 * sqrt(Sigma(0, 0)));
+        if (naeq(observed, expected)) FAIL("evaluate_derivative_Sigma");
+    }
     return true;
 }
 
@@ -130,6 +144,27 @@ bool test_degenerate_N1M2() {
         double observed = mv.get();
         double expected = fn.evaluate() + fn2.evaluate();
         if (naeq(observed, expected)) FAIL("evaluate");
+    }
+
+    {
+        // evaluate_derivative_FM
+        VectorXd observed = mv.get_derivative_FM();
+        double expected = fn.evaluate_derivative_FM();
+        if (naeq(observed(0), expected)) FAIL("evaluate_derivative_FM 1");
+        expected = fn2.evaluate_derivative_FM();
+        if (naeq(observed(1), expected)) FAIL("evaluate_derivative_FM 2");
+    }
+
+    {
+        // evaluate_derivative_Sigma
+        MatrixXd observed = mv.get_derivative_Sigma();
+        double expected =
+            fn.evaluate_derivative_sigma() / (2 * sqrt(Sigma(0, 0)));
+        if (naeq(observed(0, 0), expected))
+            FAIL("evaluate_derivative_Sigma 1 1");
+        expected = fn2.evaluate_derivative_sigma() / (2 * sqrt(Sigma(1, 1)));
+        if (naeq(observed(1, 1), expected))
+            FAIL("evaluate_derivative_Sigma 2 2");
     }
     return true;
 }
@@ -172,15 +207,53 @@ bool test_2D() {
                                    (FA(1) - FM(1)) / (sigma1 * sigma2));
         if (naeq(observed, expected)) FAIL("evaluate");
     }
+
+    {
+        // test_evaluate_derivative_FM
+        VectorXd observed = mv.get_derivative_FM();
+        double expected =
+            ((FM(1) - FA(1)) * rho * sigma1 + (FA(0) - FM(0)) * sigma2) /
+            ((-1 + rho * rho) * sigma1 * sigma1 * sigma2);
+        if (naeq(observed(0), expected)) FAIL("derivative_FM(0)");
+        expected =
+            ((FA(1) - FM(1)) * sigma1 + (FM(0) - FA(0)) * rho * sigma2) /
+            ((-1 + rho * rho) * sigma1 * sigma2 * sigma2);
+        if (naeq(observed(1), expected)) FAIL("derivative_FM(1)");
+    }
+
+    {
+        // test_evaluate_derivative_Sigma
+        double det = Sigma(0, 0) * Sigma(1, 1) - Sigma(0, 1) * Sigma(1, 0);
+        double eps1 = FA(0) - FM(0);
+        double eps2 = FA(1) - FM(1);
+        MatrixXd observed = mv.get_derivative_Sigma();
+        double expected = 0.5 / SQUARE(det) *
+                          (SQUARE(Sigma(1, 1)) * (Sigma(0, 0) - SQUARE(eps1)) -
+                           SQUARE(Sigma(0, 1)) * (Sigma(1, 1) + SQUARE(eps2)) +
+                           2 * Sigma(0, 1) * Sigma(1, 1) * eps1 * eps2);
+        if (naeq(observed(0, 0), expected)) FAIL("derivative_Sigma(0,0)");
+        expected = 0.5 / SQUARE(det) *
+                   (SQUARE(Sigma(0, 0)) * (Sigma(1, 1) - SQUARE(eps2)) -
+                    SQUARE(Sigma(1, 0)) * (Sigma(0, 0) + SQUARE(eps1)) +
+                    2 * Sigma(1, 0) * Sigma(0, 0) * eps2 * eps1);
+        if (naeq(observed(1, 1), expected)) FAIL("derivative_Sigma(1,1)");
+        expected =
+            0.5 / SQUARE(det) *
+            (Sigma(0, 1) * (-det + Sigma(1, 1) * SQUARE(eps1) +
+                            Sigma(0, 0) * SQUARE(eps2)) -
+             eps1 * eps2 * (SQUARE(Sigma(1, 0)) + Sigma(0, 0) * Sigma(1, 1)));
+        if (naeq(observed(0, 1), expected)) FAIL("derivative_Sigma(0,1)");
+        if (naeq(observed(1, 0), expected)) FAIL("derivative_Sigma(1,0)");
+    }
     return true;
 }
 }
 
 int main(int, char * []) {
     PRINT("degenerate N=1 M=1");
-    //RUNTEST(test_degenerate, 100);
+    RUNTEST(test_degenerate, 100);
     PRINT("degenerate N=1 M=2");
-    //RUNTEST(test_degenerate_N1M2, 100);
+    RUNTEST(test_degenerate_N1M2, 100);
     PRINT("2D");
     RUNTEST(test_2D, 100);
     return 0;
