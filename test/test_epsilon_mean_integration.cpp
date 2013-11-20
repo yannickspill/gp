35a39d0445c66ba2906287e1e9a6d04a1a2f83ee
Yannick <yannick.spill@pasteur.fr>
2013-11-19 19:19:41 -0800

    Test integration of vector side

--- test/test_epsilon_mean_integration.cpp
+++ test/test_epsilon_mean_integration.cpp
@@ -1,23 +1,40 @@
-#include "mvn_EpsilonVector.h"
-#include "Linear1DFunction.h"
 #include "Scalar.h"
+#include "ConstEigenObject.h"
+#include "Linear1DFunction.h"
+#include "VectorFromFunction.h"
+#include "MatrixDifference.h"
 
 #include <Eigen/Dense>
 
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+namespace{
+typedef ConstEigenObject<VectorXd> ConstVec;
+typedef VectorFromFunction<ConstVec, Linear1DFunction> VFF;
+typedef MatrixDifference<ConstVec, VFF> MD;
+}
+
+
 int main(int, char * []) {
+    //create a vector from a Linear1DFunction
+    ConstVec X(VectorXd::LinSpaced(10, 0, 9));
     Scalar a(1.0);
     Scalar b(2.0);
     Linear1DFunction f(a, b);
-    MatrixXd X(VectorXd::LinSpaced(10, 0, 9));
-    VectorXd y(VectorXd::LinSpaced(10, 0, 9));
-    EpsilonVector<Linear1DFunction> eps(X, y, f);
-    VectorXd observed(eps());
+    VFF mean(X, f);
+    //create second vector as constant
+    ConstVec y(VectorXd::LinSpaced(10, 0, 9));
+    //create difference object
+    MD eps(y, mean);
+    //
+    VectorXd observed(eps.get());
     VectorXd expected(10);
     expected.setConstant(-2.);
     if (observed != expected) return 1;
+    //
     b.set(1.0);
-    observed = eps();
+    VectorXd observed2 = eps.get();
     expected.setConstant(-1.);
-    if (observed != expected) return 2;
+    if (observed2 != expected) return 2;
     return 0;
 }
