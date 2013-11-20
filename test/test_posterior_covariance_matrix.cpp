613729cba5959232dce7c762677896c409e0681f
Yannick <yannick.spill@pasteur.fr>
2013-11-19 19:21:04 -0800

    Test construction of posterior covariance matrix

---
+++ test/test_posterior_covariance_matrix.cpp
@@ -0,0 +1,44 @@
+#include "ConstEigenObject.h"
+#include "MatrixSum.h"
+#include "MatrixProduct.h"
+#include "SymmetricMatrixFromFunction.h"
+#include "Scalar.h"
+
+#include <Eigen/Dense>
+
+class ConstCovFunc {
+   public:
+    double eval(Eigen::RowVectorXd a, Eigen::RowVectorXd b) const {
+        return 1.;
+    }
+};
+
+int main(int, char * []) {
+    //build sigma^2 * S
+    typedef ConstEigenObject<Eigen::MatrixXd> ConstMat;
+    ConstMat S(Eigen::MatrixXd::Identity(10, 10));
+    Scalar sigma(0.0);
+    typedef MatrixProduct<Scalar, ConstMat> MP;
+    MP siS(sigma, S);
+    typedef MatrixProduct<Scalar, MP> MPP;
+    MPP si2S(sigma, siS);
+    //build W
+    ConstCovFunc cc;
+    ConstMat X(Eigen::MatrixXd::Random(10, 10));
+    typedef SymmetricMatrixFromFunction<ConstMat, ConstCovFunc> MF;
+    MF W(X, cc);
+    //sum the two
+    typedef MatrixSum<MF, MPP> MS;
+    MS cov(W, si2S);
+    //test
+    Eigen::MatrixXd observed(cov.get().selfadjointView<Eigen::Upper>());
+    Eigen::MatrixXd expected(Eigen::MatrixXd::Ones(10,10));
+    if (observed != expected) return 1;
+    //change sigma and test again
+    sigma.set(3.0);
+    Eigen::MatrixXd observed2(cov.get().selfadjointView<Eigen::Upper>());
+    expected += 9.0*Eigen::MatrixXd::Identity(10,10);
+    if (observed2 != expected) return 2;
+    return 0;
+}
+
