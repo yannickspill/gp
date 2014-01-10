62ee30f26673017c46d51e219b2dff7cec9caa62
Yannick Spill <yannick@salilab.org>
2014-01-10 15:33:50 +0100

    GPMatrix compiles
    typedefs point to traits class, and traits class is specialized for GPMatrix

--- include/GPMatrix.h
+++ include/GPMatrix.h
@@ -4,19 +4,19 @@
 #include "macros.h"
 
 #include <Eigen/Dense>
-#include <iostream>
-#include <memory>
+
+// declare traits class
+template <class Derived> struct traits;
 
 //! Base class for any matrix
 template <class Derived>
 class GPMatrixBase {
 
- //public:
+ public:
   // typedefs
-  typedef Derived::SomeType rd;
-  typedef typename Derived::result_type rtype;
-  static const unsigned RowsAtCompileTime = rtype::RowsAtCompileTime;
-  static const unsigned ColsAtCompileTime = rtype::ColsAtCompileTime;
+  typedef typename traits<Derived>::result_type result_type;
+  static const unsigned RowsAtCompileTime = result_type::RowsAtCompileTime;
+  static const unsigned ColsAtCompileTime = result_type::ColsAtCompileTime;
 
  public:
   // allow casting to Derived
@@ -32,7 +32,7 @@ class GPMatrix : public GPMatrixBase<GPMatrix<EigenType> > {
   EigenType data_;
 
  public:
-  typedef EigenType result_type;
+  typedef typename GPMatrixBase<GPMatrix>::result_type result_type;
 
  public:
   //! Construct directly from Eigen expression
@@ -40,7 +40,7 @@ class GPMatrix : public GPMatrixBase<GPMatrix<EigenType> > {
 
   //! Construct from GP matrix expression, convert if needed
   template <class OtherDerived>
-  GPMatrix(const GPMatrixBase<OtherDerived>& expr)
+  explicit GPMatrix(const GPMatrixBase<OtherDerived>& expr)
       : data_(expr.eigen()) {}
 
   //! Return bare Eigen type
@@ -48,89 +48,11 @@ class GPMatrix : public GPMatrixBase<GPMatrix<EigenType> > {
   EigenType eigen() const { return data_; }
 };
 
-//! \addtogroup Matrix sum, difference, product and division templates @{
-template <typename Lhs, typename Rhs>
-class GPMatrixSum : public GPMatrixBase<GPMatrixSum<Lhs, Rhs> > {
- private:
-  Lhs lhs_;
-  Rhs rhs_;
-
- public:
-  typedef decltype(lhs_ + rhs_) result_type;
-
- public:
-  GPMatrixSum(const Lhs& lhs, const Rhs& rhs) : lhs_(lhs), rhs_(rhs) {}
-  result_type eigen() const { return lhs_ + rhs_; }
+// specialize traits for GPMatrix
+template <class EigenType>
+struct traits<GPMatrix<EigenType> > {
+  typedef EigenType result_type;
 };
-//
-template <typename Lhs, typename Rhs>
-class GPMatrixDifference : public GPMatrixBase<GPMatrixDifference<Lhs, Rhs> > {
- private:
-  Lhs lhs_;
-  Rhs rhs_;
-
- public:
-  typedef decltype(lhs_ - rhs_) result_type;
-
- public:
-  GPMatrixDifference(const Lhs& lhs, const Rhs& rhs) : lhs_(lhs), rhs_(rhs) {}
-  result_type eigen() const { return lhs_ - rhs_; }
-};
-//
-template <typename Lhs, typename Rhs>
-class GPMatrixProduct : public GPMatrixBase<GPMatrixProduct<Lhs, Rhs> > {
- private:
-  Lhs lhs_;
-  Rhs rhs_;
-
- public:
-  typedef decltype(lhs_* rhs_) result_type;
-
- public:
-  GPMatrixProduct(const Lhs& lhs, const Rhs& rhs) : lhs_(lhs), rhs_(rhs) {}
-  result_type eigen() const { return lhs_ * rhs_; }
-};
-//
-template <typename Lhs, typename Rhs>
-class GPMatrixDivision : public GPMatrixBase<GPMatrixDivision<Lhs, Rhs> > {
- private:
-  Lhs lhs_;
-  Rhs rhs_;
-
- public:
-  typedef decltype(lhs_* rhs_) result_type;
-
- public:
-  GPMatrixDivision(const Lhs& lhs, const Rhs& rhs) : lhs_(lhs), rhs_(rhs) {}
-  result_type eigen() const { return lhs_ / rhs_; }
-};
-//! @}
-
-//! \addtogroup Matrix sum, difference, product and division operators @{
-template <typename Lhs, typename Rhs>
-const GPMatrixSum<Lhs, Rhs> operator+(const GPMatrixBase<Lhs>& lhs,
-                                      const GPMatrixBase<Rhs>& rhs) {
-  return GPMatrixSum<Lhs, Rhs>(lhs, rhs);
-}
-//
-template <typename Lhs, typename Rhs>
-const GPMatrixDifference<Lhs, Rhs> operator-(const GPMatrixBase<Lhs>& lhs,
-                                             const GPMatrixBase<Rhs>& rhs) {
-  return GPMatrixDifference<Lhs, Rhs>(lhs, rhs);
-}
-//
-template <typename Lhs, typename Rhs>
-const GPMatrixProduct<Lhs, Rhs> operator*(const GPMatrixBase<Lhs>& lhs,
-                                          const GPMatrixBase<Rhs>& rhs) {
-  return GPMatrixProduct<Lhs, Rhs>(lhs, rhs);
-}
-//
-template <typename Lhs, typename Rhs>
-const GPMatrixDivision<Lhs, Rhs> operator/(const GPMatrixBase<Lhs>& lhs,
-                                           const GPMatrixBase<Rhs>& rhs) {
-  return GPMatrixDivision<Lhs, Rhs>(lhs, rhs);
-}
-//! @}
 
 //! \addtogroup Common matrix typedefs @{
 typedef GPMatrix<Eigen::MatrixXd> GPMatrixXd;
--- test/test_GPMatrix.cpp
+++ test/test_GPMatrix.cpp
@@ -2,18 +2,17 @@
 
 #include <Eigen/Dense>
 
-
-int main(int, char*[]) {
-    //matrix creation
-    Eigen::MatrixXd X(Eigen::MatrixXd::Random(10,3));
-    GPMatrixXd gpX(X);
-    if (gpX.eigen() != X) return 1;
-    //matrix sum
-    Eigen::MatrixXd Y(Eigen::MatrixXd::Random(10,3));
-    GPMatrixXd gpY(Y);
-    GPMatrixXd gpSum(X+Y);
-    if (gpSum.eigen() != X+Y) return 2;
-    //gpY*2.;
-    //if ((gpX+gpY).eigen() != X+Y) return 3;
-    return 0;
+int main(int, char * []) {
+  Eigen::MatrixXd X(Eigen::MatrixXd::Random(10, 3));
+  GPMatrixXd gpX(X); // matrix creation
+  if (gpX.eigen() != X) return 1;
+  Eigen::MatrixXd Y(Eigen::MatrixXd::Random(10, 3));
+  GPMatrixXd gpSum(X + Y);
+  if (gpSum.eigen() != X + Y) return 2; //make sure nothing changes
+  GPMatrixXd gpY(Y);
+  gpY;
+  //gpX + gpY; //type is defined
+  // if ((gpX+gpY).eigen() != X+Y) return 3; // + is properly implemented
+  // gpSum = gpX + gpY; //type conversion
+  return 0;
 }
