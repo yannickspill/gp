ae181b86a6f28aa78de644d021ed478d3e0e26a0
Yannick <yannick.spill@pasteur.fr>
2014-01-19 13:43:20 +0100

    rewrite determinant to be a specialization for LDLTPolicy

--- include/internal/LogDeterminant.h
+++ include/internal/LogDeterminant.h
@@ -3,7 +3,6 @@
 
 #include "macros.h"
 #include "internal/MatrixBase.h"
-#include "internal/Matrix.h"
 
 #include <Eigen/Core>
 #include <type_traits>
@@ -12,33 +11,37 @@
 namespace GP {
 namespace internal {
 
+// traits are general because a determinant will always be a double
 template <class Derived> struct traits<LogDeterminant<Derived> > {
   typedef typename Derived::scalar_type scalar_type;
-  typedef double result_type;
+  typedef scalar_type result_type;
 };
 
-//determinant of a matrix, specialization for the LDLT case
-template <typename Derived>
-class LogDeterminant : public MatrixBase<LogDeterminant<Derived> > {
+// determinant of a matrix, specialization for the LDLT case
+template <class DerivedMat>
+class LogDeterminant
+    <Decomposition
+     <DerivedMat, LDLTPolicy> > : public MatrixBase
+                                  <LogDeterminant
+                                   <Decomposition<DerivedMat, LDLTPolicy> > > {
  public:
   typedef typename traits<LogDeterminant>::scalar_type scalar_type;
-  typedef typename traits <LogDeterminant>::result_type result_type;
+  typedef typename traits<LogDeterminant>::result_type result_type;
 
  private:
-  Derived data_;
+  Decomposition<DerivedMat, LDLTPolicy> data_;
 
  public:
   // constructor
-  LogDeterminant(const Derived& data) : data_(data) {}
+  LogDeterminant(const Decomposition<DerivedMat, LDLTPolicy>& data)
+      : data_(data) {}
 
   // actual computation
   result_type get() const {
     return data_.get().vectorD().array().abs().log().sum();
   }
 
-  unsigned get_version() const {
-    return data_.get_version();
-  }
+  unsigned get_version() const { return data_.get_version(); }
 };
 }
 }
--- include/internal/MatrixBase.h
+++ include/internal/MatrixBase.h
@@ -30,7 +30,7 @@ template <class Derived> class MatrixBase : public GPBase<Derived> {
   }
 
   // log-determinant, can only be applied to LDLT decomposition for now
-  // write another specialization in Determinant.h if you need more
+  // write another specialization in LogDeterminant.h if you need more
   LogDeterminant<Derived> logdet() const {
     return LogDeterminant<Derived>(asDerived());
   }
