8bfffd45c4ea3f8ebc4a88adc49e93cdc4e74bfb
Yannick <yannick.spill@pasteur.fr>
2014-01-20 11:57:35 +0100

    simplify inclusion of Scalar/Matrix ops

--- test/test_make_function.cpp
+++ test/test_make_function.cpp
@@ -1,24 +1,21 @@
-#include "internal/Scalar.h"
-#include "internal/ScalarScalarProduct.h"
-#include "internal/MatrixScalarProduct.h"
-#include "internal/MatrixMatrixProduct.h"
-#include "internal/Matrix.h"
+#include "Scalar.h"
+#include "Matrix.h"
 
 #include "internal/make_function.h"
 
 #include <Eigen/Dense>
 #include <iostream>
 
-using namespace GP::internal;
+using namespace GP;
 
 int main(int, char*[]){
     Scalar x(1.0);
     Scalar y(2.0);
     auto xy = x*y;
-    auto f1 = make_function(xy, x);
+    auto f1 = internal::make_function(xy, x);
     if (f1(1.0) != 2.0) return 1;
     if (f1(2.0) != 4.0) return 2;
-    auto f2 = make_function(x*y, x, y);
+    auto f2 = internal::make_function(x*y, x, y);
     if (f2(1.0,5.0) != 5.0) return 3;
     if (f2(2,-5.0) != -10.0) return 4;
     Eigen::MatrixXd mat(Eigen::MatrixXd::Random(3,4));
@@ -27,7 +24,7 @@ int main(int, char*[]){
     RowVectorXd gplvec(lvec);
     Eigen::VectorXd rvec(Eigen::VectorXd::LinSpaced(4,0,1));
     VectorXd gprvec(rvec);
-    auto f3 = make_function(gplvec*gpmat*gprvec, gplvec);
+    auto f3 = internal::make_function(gplvec*gpmat*gprvec, gplvec);
     if (f3(lvec) != lvec*mat*rvec) return 5;
 
     return 0;
