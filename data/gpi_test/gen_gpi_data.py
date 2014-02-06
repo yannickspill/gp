e085bcfb70769bea876d6d2197baf487920a32dc
Yannick <yannick.spill@pasteur.fr>
2014-02-06 11:06:02 +0100

    output parameters as well

--- data/gpi_test/gen_gpi_data.py
+++ data/gpi_test/gen_gpi_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys,os
+import sys
 
 from numpy import *
 
@@ -8,37 +8,58 @@ import IMP
 import IMP.core
 import IMP.isd
 
-#data
-npoints = 11 # q values
-ndraws = 10 # times we evaluate gpir for each of tau lambda and sigma
+if len(sys.argv) != 3:
+    sys.exit("""args: param.txt vals.txt""")
+
+# data
+npoints = 11  # q values
+ndraws = 10  # times we evaluate gpir for each of tau lambda and sigma
 logmin = -2
-logmax = 2 # range on log scale for nuisance values
-q = linspace(-10,10,num=npoints)
+logmax = 2  # range on log scale for nuisance values
+q = linspace(-10, 10, num=npoints)
 I = random.normal(q) + q**2
-q = [[i] for i in q ]
+q = [[i] for i in q]
 err = ones(npoints)
 N = 10
 
-#functions
+# functions
 m = IMP.Model()
-a = IMP.isd.Nuisance.setup_particle(IMP.Particle(m),0.0)
-b = IMP.isd.Nuisance.setup_particle(IMP.Particle(m),0.0)
-mean = IMP.isd.Linear1DFunction(a,b)
-tausig = IMP.isd.Scale.setup_particle(IMP.Particle(m),1.0)
-lam = IMP.isd.Scale.setup_particle(IMP.Particle(m),1.0)
-sig2 = IMP.isd.Scale.setup_particle(IMP.Particle(m),1.0)
+a = IMP.isd.Nuisance.setup_particle(IMP.Particle(m), 0.0)
+b = IMP.isd.Nuisance.setup_particle(IMP.Particle(m), 0.0)
+mean = IMP.isd.Linear1DFunction(a, b)
+tausig = IMP.isd.Scale.setup_particle(IMP.Particle(m), 1.0)
+lam = IMP.isd.Scale.setup_particle(IMP.Particle(m), 1.0)
+sig2 = IMP.isd.Scale.setup_particle(IMP.Particle(m), 1.0)
 cov = IMP.isd.Covariance1DFunction(tausig, lam)
 
-#gpi
+# gpi
 gpi = IMP.isd.GaussianProcessInterpolation(q, I.tolist(),
-                                err.tolist(), N, mean, cov, sig2)
+                                           err.tolist(), N, mean, cov, sig2)
 gpir = IMP.isd.GaussianProcessInterpolationRestraint(m, gpi)
 sf = IMP.core.RestraintsScoringFunction([gpir])
 
-print "#a b sigma tau lambda score"
-nuisances = [sig2,tausig,lam]
+fl = open(sys.argv[1], 'w')
+fl.write('#in order: npoints, q, I, err, N, ndraws, values\n')
+fl.write('%d\n' % npoints)
+for i in q:
+    fl.write('%G ' % i[0])
+fl.write('\n')
+for i in [I, err]:
+    for j in i:
+        fl.write('%G ' % j)
+    fl.write('\n')
+fl.write('%d\n' % N)
+fl.write('%d\n' % ndraws)
+for i in logspace(logmin, logmax, num=ndraws):
+    fl.write('%G ' % i)
+fl.write('\n')
+
+
+fl = open(sys.argv[2], 'w')
+fl.write("#a b sigma tau lambda score\n")
+nuisances = [sig2, tausig, lam]
 for nuis in nuisances:
-    for val in logspace(logmin,logmax,num=ndraws):
+    for val in logspace(logmin, logmax, num=ndraws):
         nuis.set_scale(val)
         ene = sf.evaluate(False)
         aval = a.get_nuisance()
@@ -46,13 +67,7 @@ for nuis in nuisances:
         sigval = sig2.get_scale()**0.5
         tauval = tausig.get_scale()/sigval
         lamval = lam.get_scale()
-        print aval,bval,sigval,tauval,lamval,ene
+        for i in [aval, bval, sigval, tauval, lamval, ene]:
+            fl.write('%G ' % i)
+        fl.write('\n')
     nuis.set_scale(1.0)
-
-
-
-
-
-
-
-
