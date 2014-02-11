f4bc49c30078aa37e09adfbfce8985ee508848ba
Yannick <yannick.spill@pasteur.fr>
2014-02-11 12:43:37 +0100

    create test for new bug

--- test/test_SymmetricMatrixFromBivariateFunctor.cpp
+++ test/test_SymmetricMatrixFromBivariateFunctor.cpp
@@ -1,5 +1,6 @@
 #include "Scalar.h"
 #include "Matrix.h"
+#include "covariance_functions.h"
 
 #include <Eigen/Dense>
 #include <iostream>
@@ -37,5 +38,11 @@ int main(int, char*[]){
          - Eigen::MatrixXd(expected2.selfadjointView<Eigen::Upper>())).norm()
         > 1e-5)
       return 2;
+    //product with a scalar
+    Scalar lambda(1.0);
+    auto f3 = GP::covariance::squared_exponential(lambda);
+    GP::VectorXd inVec2(Eigen::VectorXd::LinSpaced(11,0,1));
+    auto m3 = MatrixXd::SymmetricApply(f3,inVec2);
+    if ((1.*m3).get() != m3.get()) return 3;
     return 0;
 }
